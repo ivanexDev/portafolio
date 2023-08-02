11fca0154a63a13647b56d680b7db9123419b813
Ivan Pereira <ivanexdev@gmail.com>
2023-08-02 12:03:55 -0400

    test browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Aboutme } from "./pages/Aboutme";
 import { Projects } from "./pages/Projects";
@@ -10,7 +10,7 @@ import { Error404 } from "./pages/Error404";
 function App() {
 	return (
 		<>
-			<BrowserRouter>
+		
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<Home />} />
@@ -18,7 +18,7 @@ function App() {
 					<Route path="/projects" element={<Projects />} />
 					<Route path="*" element={<Error404 />} />
 				</Routes>
-			</BrowserRouter>
+			
 		</>
 	);
 }
